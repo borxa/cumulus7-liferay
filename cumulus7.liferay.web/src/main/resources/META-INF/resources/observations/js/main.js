cbc84dda9669fbd23af30c834bf7a8b4d43dc9a4
borxa <borxa@192.168.1.37>
2016-06-19 09:36:07 +0200

    Refactoring javascript

--- cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
+++ cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
@@ -4,68 +4,70 @@ AUI().ready('node', 'aui-io-request',
                     function () {
                         this.on('click',
                                 function () {
-                                    getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                                    getJSON(A);
                                 });
                     });
             A.on('load',
                     function () {
-                        getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                        getJSON(A);
                         setInterval(function () {
-                            getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                            getJSON(A);
                         }, 180000);
                     }
             );
-            function getJSON(jsonURL) {
-                A.io.request(jsonURL, {
-                    dataType: 'json',
-                    on: {
-                        success: function () {
-                            var data = this.get('responseData');
-                            updateWeather(data);
-                        }
-                    }
-                });
-            }
-            function updateWeather(json) {
-                A.all('.temp').each(function () {
-                    var value = json.temp.replace(',', '.');
-                    var valueUnit = json.tempunit;
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.intemp').each(function () {
-                    var value = json.intemp.replace(',', '.');
-                    var valueUnit = json.tempunit;
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.hum').each(function () {
-                    var value = json.hum.replace(',', '.');
-                    var valueUnit = '%';
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.inhum').each(function () {
-                    var value = json.inhum.replace(',', '.');
-                    var valueUnit = '%';
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.wspeed').each(function () {
-                    var value = json.wgust.replace(',', '.');
-                    var valueUnit = json.windunit;
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.currentwdir').each(function () {
-                    var value = json.wgust.replace(',', '.');
-                    var valueUnit = json.windunit;
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.rhour').each(function () {
-                    var value = json.rhour.replace(',', '.');
-                    var valueUnit = json.rainunit;
-                    this.html(value + ' ' + valueUnit);
-                });
-                A.all('.timehhmmss').each(function () {
-                    var value = json.timehhmmss;
-                    this.html(value);
-                });
-            }
         }
 );
+
+function getJSON(A) {
+    A.io.request(A.one('.observations-json-link').getAttribute('data-href'), {
+        dataType: 'json',
+        on: {
+            success: function () {
+                var data = this.get('responseData');
+                updateWeather(A, data);
+            }
+        }
+    });
+}
+
+function updateWeather(A, json) {
+    A.all('.temp').each(function () {
+        var value = json.temp.replace(',', '.');
+        var valueUnit = json.tempunit;
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.intemp').each(function () {
+        var value = json.intemp.replace(',', '.');
+        var valueUnit = json.tempunit;
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.hum').each(function () {
+        var value = json.hum.replace(',', '.');
+        var valueUnit = '%';
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.inhum').each(function () {
+        var value = json.inhum.replace(',', '.');
+        var valueUnit = '%';
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.wspeed').each(function () {
+        var value = json.wgust.replace(',', '.');
+        var valueUnit = json.windunit;
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.currentwdir').each(function () {
+        var value = json.wgust.replace(',', '.');
+        var valueUnit = json.windunit;
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.rhour').each(function () {
+        var value = json.rhour.replace(',', '.');
+        var valueUnit = json.rainunit;
+        this.html(value + ' ' + valueUnit);
+    });
+    A.all('.timehhmmss').each(function () {
+        var value = json.timehhmmss;
+        this.html(value);
+    });
+}
