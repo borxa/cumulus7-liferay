40a0cbacfc35b17f3602a986a3862b8d2dc82806
borxa <borxa@192.168.1.37>
2016-06-19 09:16:56 +0200

    Refactoring javascript

--- cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
+++ cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
@@ -4,81 +4,107 @@ AUI().ready(
         'aui-io-request',
         'aui-progressbar',
         function (A) {
-            A.all('.observations-json-link').each(
-                    function () {
-                        this.on('click',
-                                function () {
-                                    refreshData(A);
-                                });
-                    });
-            A.on('load',
-                    function () {
-                        refreshData(A);
-                        setInterval(function () {
-                            refreshData(A);
-                        }, 180000);
+            var jsonURL = A.one('.observations-json-link').getAttribute('data-href');
+            if (jsonURL) {
+                A.all('.observations-json-link').each(
+                        function () {
+                            this.on('click',
+                                    function () {
+                                        getJSON(jsonURL);
+                                    });
+                        });
+                A.on('load',
+                        function () {
+                            getJSON(jsonURL);
+                            setInterval(function () {
+                                getJSON(jsonURL);
+                            }, 180000);
+                        }
+                );
+            }
+            function getJSON(jsonURL) {
+                A.io.request(jsonURL, {
+                    dataType: 'json',
+                    on: {
+                        success: function () {
+                            var data = this.get('responseData');
+                            updateWeather(data);
+                        }
                     }
-            );
+                });
+            }
+            function updateWeather(json) {
+                // EXTERIOR TEMPERATURE
+                updateTemp(json);
+                // INTERIOR TEMPERATURE
+                updateInTemp(json);
+                // EXTERIOR HUMIDITY
+                updateHum(json);
+                // INTERIOR HUMIDITY
+                updateInHum(json);
+                // WIND SPEED
+                updateWindSpeed(json);
+                // CURRENT WIND DIRECTION
+                updateWindDirection(json);
+                // RAINFALL LAST HOUR
+                updateRainFall(json);
+                // TIME
+                updateTime(json);
+            }
+            function updateTemp(json) {
+                A.all('.temp').each(function () {
+                    var value = json.temp.replace(',', '.');
+                    var valueUnit = json.tempunit;
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateInTemp(json) {
+                A.all('.intemp').each(function () {
+                    var value = json.intemp.replace(',', '.');
+                    var valueUnit = json.tempunit;
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateHum(json) {
+                A.all('.hum').each(function () {
+                    var value = json.hum.replace(',', '.');
+                    var valueUnit = '%';
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateInHum(json) {
+                A.all('.inhum').each(function () {
+                    var value = json.inhum.replace(',', '.');
+                    var valueUnit = '%';
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateWindSpeed(json) {
+                A.all('.wspeed').each(function () {
+                    var value = json.wgust.replace(',', '.');
+                    var valueUnit = json.windunit;
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateWindDirection(json) {
+                A.all('.currentwdir').each(function () {
+                    var value = json.wgust.replace(',', '.');
+                    var valueUnit = json.windunit;
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateRainFall(json) {
+                A.all('.rhour').each(function () {
+                    var value = json.rhour.replace(',', '.');
+                    var valueUnit = json.rainunit;
+                    this.html(value + ' ' + valueUnit);
+                });
+            }
+            function updateTime(json) {
+                A.all('.timehhmmss').each(function () {
+                    var value = json.timehhmmss;
+                    this.html(value);
+                });
+            }
         }
 );
-
-function refreshData(A) {
-    var jsonURL = A.one('.observations-json-link').getAttribute('data-href');
-    if (jsonURL) {
-        A.io.request(jsonURL, {
-            dataType: 'json',
-            on: {
-                success: function () {
-                    var data = this.get('responseData');
-                    // EXTERIOR TEMPERATURE
-                    A.all('.temp').each(function () {
-                        var value = data.temp.replace(',', '.');
-                        var valueUnit = data.tempunit;
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // INTERIOR TEMPERATURE
-                    A.all('.intemp').each(function () {
-                        var value = data.intemp.replace(',', '.');
-                        var valueUnit = data.tempunit;
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // EXTERIOR HUMIDITY
-                    A.all('.hum').each(function () {
-                        var value = data.hum.replace(',', '.');
-                        var valueUnit = '%';
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // INTERIOR HUMIDITY
-                    A.all('.inhum').each(function () {
-                        var value = data.inhum.replace(',', '.');
-                        var valueUnit = '%';
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // WIND SPEED
-                    A.all('.wspeed').each(function () {
-                        var value = data.wgust.replace(',', '.');
-                        var valueUnit = data.windunit;
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // CURRENT WIND DIRECTION
-                    A.all('.currentwdir').each(function () {
-                        var value = data.wgust.replace(',', '.');
-                        var valueUnit = data.windunit;
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // RAINFALL LAST HOUR
-                    A.all('.rhour').each(function () {
-                        var value = data.rhour.replace(',', '.');
-                        var valueUnit = data.rainunit;
-                        this.html(value + ' ' + valueUnit);
-                    });
-                    // TIME
-                    A.all('.timehhmmss').each(function () {
-                        var value = data.timehhmmss;
-                        this.html(value);
-                    });
-                }
-            }
-        });
-    }
-}
