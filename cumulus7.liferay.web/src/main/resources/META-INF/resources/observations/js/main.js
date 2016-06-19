43ab5c50d6aafe7755a823271b2fd169b18ae290
borxa <borxa@192.168.1.37>
2016-06-19 09:28:19 +0200

    Refactoring javascript

--- cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
+++ cumulus7.liferay.web/src/main/resources/META-INF/resources/observations/js/main.js
@@ -1,27 +1,20 @@
-AUI().ready(
-        'node',
-        'liferay-portlet-url',
-        'aui-io-request',
-        'aui-progressbar',
+AUI().ready('node', 'aui-io-request',
         function (A) {
-            var jsonURL = A.one('.observations-json-link').getAttribute('data-href');
-            if (jsonURL) {
-                A.all('.observations-json-link').each(
-                        function () {
-                            this.on('click',
-                                    function () {
-                                        getJSON(jsonURL);
-                                    });
-                        });
-                A.on('load',
-                        function () {
-                            getJSON(jsonURL);
-                            setInterval(function () {
-                                getJSON(jsonURL);
-                            }, 180000);
-                        }
-                );
-            }
+            A.all('.observations-json-link').each(
+                    function () {
+                        this.on('click',
+                                function () {
+                                    getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                                });
+                    });
+            A.on('load',
+                    function () {
+                        getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                        setInterval(function () {
+                            getJSON(A.one('.observations-json-link').getAttribute('data-href'));
+                        }, 180000);
+                    }
+            );
             function getJSON(jsonURL) {
                 A.io.request(jsonURL, {
                     dataType: 'json',
@@ -34,73 +27,41 @@ AUI().ready(
                 });
             }
             function updateWeather(json) {
-                // EXTERIOR TEMPERATURE
-                updateTemp(json);
-                // INTERIOR TEMPERATURE
-                updateInTemp(json);
-                // EXTERIOR HUMIDITY
-                updateHum(json);
-                // INTERIOR HUMIDITY
-                updateInHum(json);
-                // WIND SPEED
-                updateWindSpeed(json);
-                // CURRENT WIND DIRECTION
-                updateWindDirection(json);
-                // RAINFALL LAST HOUR
-                updateRainFall(json);
-                // TIME
-                updateTime(json);
-            }
-            function updateTemp(json) {
                 A.all('.temp').each(function () {
                     var value = json.temp.replace(',', '.');
                     var valueUnit = json.tempunit;
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateInTemp(json) {
                 A.all('.intemp').each(function () {
                     var value = json.intemp.replace(',', '.');
                     var valueUnit = json.tempunit;
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateHum(json) {
                 A.all('.hum').each(function () {
                     var value = json.hum.replace(',', '.');
                     var valueUnit = '%';
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateInHum(json) {
                 A.all('.inhum').each(function () {
                     var value = json.inhum.replace(',', '.');
                     var valueUnit = '%';
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateWindSpeed(json) {
                 A.all('.wspeed').each(function () {
                     var value = json.wgust.replace(',', '.');
                     var valueUnit = json.windunit;
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateWindDirection(json) {
                 A.all('.currentwdir').each(function () {
                     var value = json.wgust.replace(',', '.');
                     var valueUnit = json.windunit;
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateRainFall(json) {
                 A.all('.rhour').each(function () {
                     var value = json.rhour.replace(',', '.');
                     var valueUnit = json.rainunit;
                     this.html(value + ' ' + valueUnit);
                 });
-            }
-            function updateTime(json) {
                 A.all('.timehhmmss').each(function () {
                     var value = json.timehhmmss;
                     this.html(value);
